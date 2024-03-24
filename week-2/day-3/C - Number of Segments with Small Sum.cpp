85ee2c85e0a1d4171759bac456d59055622e0ada
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-03-24 16:50:54 +0600

    Create C - Number of Segments with Small Sum.cpp

---
+++ week-2/day-3/C - Number of Segments with Small Sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll n, s;
+  cin >> n >> s;
+  ll a[n];
+  for (ll i = 0; i < n; i++)
+    cin >> a[i];
+  ll i = 0, j = 0;
+  ll sum = 0, ans = 0;
+  while (j < n)
+  {
+    sum += a[j];
+    while (sum > s)
+    {
+      sum -= a[i];
+      i++;
+    }
+    ans += j - i + 1;
+    j++;
+  }
+  cout << ans;
+}
