11fc82d6dad6bd6d9f429f49d70cf6f6199b599b
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-04-17 22:18:08 +0600

    Create N - Dull Operation.cpp

---
+++ week-4/day-6/N - Dull Operation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define tc int t;cin>>t;while(t--)
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    tc {
+        int n; cin>>n;
+        cout<<n-1<<' '<<n<<endl;
+    }
+	return 0;
+}
