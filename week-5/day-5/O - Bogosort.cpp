bde8dcbd74e398aaa3aee814c71812d8bda2a8d3
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-04-22 02:48:35 +0600

    Create O - Bogosort.cpp

---
+++ week-5/day-5/O - Bogosort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        sort(v.rbegin(), v.rend());
+        for (auto u : v)
+            cout << u << " ";
+        cout << endl;
+    }
+}
