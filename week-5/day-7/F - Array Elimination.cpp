fbc17bb561cf6c67ec3e5a926c2b7f998e32efe6
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-04-25 11:42:17 +0600

    Create F - Array Elimination.cpp

---
+++ week-5/day-7/F - Array Elimination.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+
+string decToBinary(int n)
+{
+    string ans;
+    for (int i = 64; i >= 0; i--)
+    {
+        int k = n >> i;
+        if (k & 1)
+            ans += '1';
+        else
+            ans += '0';
+    }
+    return ans;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        set<int> s;
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        vector<int> m(64);
+        int g = 0;
+        for (int i = 0; i < n; i++)
+        {
+            string s = decToBinary(v[i]);
+            for (int j = 0; j < 64; j++)
+            {
+                if (s[j] != '0')
+                    m[j]++;
+            }
+        }
+        for (int i = 0; i < 64; i++)
+            g = __gcd(g, m[i]);
+        for (int i = 1; i <= n; i++)
+        {
+            if (g % i == 0)
+                cout << i << ' ';
+        }
+        cout << endl;
+    }
+}
