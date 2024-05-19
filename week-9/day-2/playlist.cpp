510bcabfcf3d7dbb1a37f217c79465188439b90f
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-05-20 00:49:53 +0600

    Update playlist.cpp

--- week-9/day-2/playlist.cpp
+++ week-9/day-2/playlist.cpp
@@ -1,1 +1,40 @@
 
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    ll ans = 0;
+    ll i = 0, j = 0;
+    unordered_set<int> s;
+
+    while (i < n && j < n)
+    {
+        if (i < n && !s.count(v[i]))
+        {
+            s.insert(v[i]);
+            ans = max(ans, i - j + 1);
+            i++;
+        }
+        while (i < n && s.count(v[i]))
+        {
+            s.erase(v[j]);
+            j++;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
