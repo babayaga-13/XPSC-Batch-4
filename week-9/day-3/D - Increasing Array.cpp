c7706f60fa2d323b75dd16929da2218eb6fa0316
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-05-21 01:44:17 +0600

    Update D - Increasing Array.cpp

--- week-9/day-3/D - Increasing Array.cpp
+++ week-9/day-3/D - Increasing Array.cpp
@@ -1,1 +1,25 @@
-
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    ll ans = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > a[i + 1])
+        {
+            ans += (a[i] - a[i + 1]);
+            a[i + 1] = a[i];
+        }
+    }
+    cout << ans;
+}
