864b82eb28e6944791a552032b496e00b245c37e
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-05-21 01:43:50 +0600

    Update E - Permutations.cpp

--- week-9/day-3/E - Permutations.cpp
+++ week-9/day-3/E - Permutations.cpp
@@ -1,1 +1,30 @@
 
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n;
+    cin >> n;
+    vector<int> v;
+    if (n < 4 && n != 1)
+        cout << "NO SOLUTION";
+    else
+    {
+        for (int i = 2; i <= n; i += 2)
+        {
+            v.push_back(i);
+        }
+        for (int i = 1; i <= n; i += 2)
+        {
+            v.push_back(i);
+        }
+
+        for (auto u : v)
+            cout << u << " ";
+    }
+}
