5f4a888636adfa6b4c38eb1c7e775a39ae312fd6
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-06-13 00:22:45 +0600

    Create I.Morning Jogging.cpp

---
+++ week-12/day-5/I.Morning Jogging.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        int a[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> a[i][j];
+        for (int i = 0; i < n; i++)
+            sort(a[i], a[i] + m);
+        for (int i = m - 1; i >= 1; i--)
+        {
+            int p = 0;
+            for (int j = 0; j < n; j++)
+                if (a[j][0] < a[p][0])
+                    p = j;
+            rotate(a[p], a[p] + 1, a[p] + i + 1);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
