019870185ab8c3eafd6a54f45288af19325c58e4
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-06-24 22:23:15 +0600

    Create B - LIS or Reverse LIS?.cpp

---
+++ week-13/day-3/B - LIS or Reverse LIS?.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            mp[a[i]]++;
+        }
+        ll c = 0, ans = 0;
+        for (auto u : mp)
+        {
+            if (u.second == 1)
+                c++;
+            else
+                ans++;
+        }
+        ans += (c + 1) / 2;
+        cout << ans << endl;
+    }
+    return 0;
+}
