69b64d42e507d61a2e72eaa1491e4babf4230f58
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-07-03 14:05:34 +0600

    Create I - Yarik and Musical Notes.cpp

---
+++ week-14/day-5/I - Yarik and Musical Notes.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        map<int, int> mp;
+        ll ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            ans += mp[x];
+            if (x == 1)
+                ans += mp[2];
+            if (x == 2)
+                ans += mp[1];
+            mp[x]++;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
