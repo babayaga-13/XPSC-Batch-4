ee8bf63e93b3205df9b9fe12a4c546e8023bd5fd
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-07-09 01:07:48 +0600

    Create G - Paranoid String.cpp

---
+++ week-15/day-2/G - Paranoid String.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll ans = n;
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i] != s[i - 1])
+                ans += i;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
