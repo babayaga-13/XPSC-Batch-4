480c8b9f61c6ed721ed927445488f2677f332c6a
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2024-08-19 23:31:04 +0600

    Create J - Mad MAD Sum.cpp

---
+++ week-17/day-3/J - Mad MAD Sum.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ln cout << endl
+#define all(v) v.begin(), v.end()
+#define count_one(x) __builtin_popcount(x)
+#define trailing_zero(x) __builtin_ctz(x)
+#define leading_zero(x) __builtin_clz(x)
+#define gcd __gcd
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int N = 1e9 + 7;
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+ll power(ll a, ll n)
+{
+    ll ans = 1;
+    while (n)
+    {
+        if (n % 2)
+            ans = (ans * a) % N, n--;
+        else
+            a = (a * a) % N, n /= 2;
+    }
+    return ans;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    ll a[n];
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        ans += a[i];
+    }
+
+    map<ll, ll> mp;
+    ll m = 0;
+    for (int i = 0; i < n; i++)
+    {
+        mp[a[i]]++;
+        if (mp[a[i]] == 2)
+        {
+            m = max(m, a[i]);
+        }
+        a[i] = m;
+        ans += a[i];
+    }
+    mp.clear();
+    m = 0;
+    for (int i = 0; i < n; i++)
+    {
+        mp[a[i]]++;
+        if (mp[a[i]] == 2)
+        {
+            m = max(m, a[i]);
+        }
+        a[i] = m;
+    }
+
+    for (int i = 0; i < n; i++)
+        ans += (n - i) * a[i];
+    cout << ans << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
